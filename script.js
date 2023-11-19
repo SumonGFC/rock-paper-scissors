c3141f9c5eaa88dae559a62dea73ae64ad1cc472
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2023-11-19 10:51:01 -0500

    Add initial outline for code/program structure

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+OUTLINE:
+1. Prompt: Would you like to play? If so, how many rounds?
+  1.a) If yes, go to 2.
+  1.b) If no, print sad face and prompt again
+2. Ask & Record user input; Generate computer choice
+3. Determine round winner; keep track of score
+4. Repeat for specified number of rounds
+5. Determine game winner;
+*/
