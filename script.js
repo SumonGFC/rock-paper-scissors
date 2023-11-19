d6811ac2d038665444f233efece9f48b2566ee63
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2023-11-19 12:07:17 -0500

    Add pseudocode

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-/*
-OUTLINE:
+/* OUTLINE:
 1. Prompt: Would you like to play? If so, how many rounds?
   1.a) If yes, go to 2.
   1.b) If no, print sad face and prompt again
@@ -7,4 +6,26 @@ OUTLINE:
 3. Determine round winner; keep track of score
 4. Repeat for specified number of rounds
 5. Determine game winner;
+
+PSEUDOCODE:
+
+//functions:
+- initializeGame()
+- getUserChoice()
+- generateComputerChoice()
+- determineRoundWinner()
+- determineGameWinner()
+
+while gameState == 0:
+prompt("Would you like to play?")
+  if yes: rounds = prompt("how many rounds?"); gameState = 1;
+  if no: print("It's okay; not everyone is brave enough to face me!");
+
+for i=1,...,rounds:
+  
+  x = getUserChoice();
+  y = generateComputerChoice(x);
+  determineRoundWinner(x, y);
+
+determineGameWinner();
 */
