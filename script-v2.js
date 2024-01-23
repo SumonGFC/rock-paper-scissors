9706254ea000f74cd958a039e9a61193bb44de6c
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-01-23 17:55:51 -0500

    Add playRound() function to script-v2

--- script-v2.js
+++ script-v2.js
@@ -1,14 +1,37 @@
-
-const USER_WIN = true;
-const USER_LOSE = false;
-const TIE = null;
 const ROCK = 'rock'; 
 const PAPER = 'paper';
 const  SCISSORS = 'scissors';
 
 function getComputerChoice() {
-    let x = Math.floor(Math.random()*3)
+    const x = Math.floor(Math.random()*3)
     if (x === 0) return ROCK;
-    else if (x === 1) return PAPER;
-    else return SCISSORS;
+    if (x === 1) return PAPER;
+    return SCISSORS;
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    const WIN = `Congrats! You win! ${playerSelection} beats ${computerSelection}!`;
+    const LOSE = `You lose! ${computerSelection} beats ${playerSelection}!`;
+    const TIE = `Tie! You both picked ${playerSelection}!`;
+
+    if (playerSelection === computerSelection) return TIE;
+
+    if (playerSelection === ROCK){
+        if (computerSelection === PAPER) return LOSE;
+        return WIN; //rock beats scissors
+    }
+
+    if (playerSelection === PAPER) {
+        if (computerSelection === SCISSORS) return LOSE;
+        return WIN; //Paper beats rock
+    }
+    
+    if (playerSelection === SCISSORS) {
+        if (computerSelection === ROCK) return LOSE;
+        return WIN; //scissors beats paper
+    }
+    
+    return "Error in playRound --- passed invalid playerSelection";   
 }
