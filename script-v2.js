ae11fafe3e966b9d25a2733437a74b3e25d9ade9
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-01-23 19:08:36 -0500

    Refactor playRound() to return booleans to be used in the game() function

--- script-v2.js
+++ script-v2.js
@@ -12,29 +12,51 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
 
-    const WIN = `Congrats! You win! ${playerSelection} beats ${computerSelection}!`;
-    const LOSE = `You lose! ${computerSelection} beats ${playerSelection}!`;
-    const TIE = `Tie! You both picked ${playerSelection}! Let's play again.`;
+    const WIN = true;
+    const LOSE = false;
+    const WIN_MSG = `You win! ${playerSelection} beats ${computerSelection}!`;
+    const LOSE_MSG = `You lose! ${computerSelection} beats ${playerSelection}!`;
+    const TIE_MSG = `Tie! We both picked ${playerSelection}! Let's play again.`;
 
     if (playerSelection === computerSelection) {
-        console.log(TIE);
+        console.log(TIE_MSG);
         return playRound(prompt("Make your choice:"), getComputerChoice());
     }
-
     if (playerSelection === ROCK) {
-        if (computerSelection === PAPER) return LOSE;
+        if (computerSelection === PAPER) {
+            console.log(LOSE_MSG);
+            return LOSE;
+        }
+        console.log(WIN_MSG)
         return WIN; //rock beats scissors
     }
-
     if (playerSelection === PAPER) {
-        if (computerSelection === SCISSORS) return LOSE;
+        if (computerSelection === SCISSORS) {
+            console.log(LOSE_MSG);
+            return LOSE;
+        }
+        console.log(WIN_MSG)
         return WIN; //Paper beats rock
     }
-    
     if (playerSelection === SCISSORS) {
-        if (computerSelection === ROCK) return LOSE;
+        if (computerSelection === ROCK) {
+            console.log(LOSE_MSG);
+            return LOSE;
+        }
+        console.log(WIN_MSG)
         return WIN; //scissors beats paper
     }
-    
-    return "Error in playRound --- passed invalid playerSelection";   
+    return playRound(
+        prompt("You seem to have made an invalid choice. Please choose again:"),
+        getComputerChoice());   
+}
+
+function game() {
+    const ROUNDS = 5;
+    let userScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < ROUNDS; ++i) {
+        if (playRound(prompt("Make your choice:"), getComputerChoice()))
+    }
 }
