515b44c78459b1a60e214c2f920a44f2690eacfa
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-01-24 22:12:06 -0500

    Refactor playRound() to update results div in html

--- script-v2.js
+++ script-v2.js
@@ -27,43 +27,39 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-
     const WIN = true;
     const LOSE = false;
+    const TIE = null;
     const WIN_MSG = `You win! ${playerSelection} beats ${computerSelection}!`;
     const LOSE_MSG = `You lose! ${computerSelection} beats ${playerSelection}!`;
     const TIE_MSG = `Tie! We both picked ${playerSelection}! Let's play again.`;
 
     if (playerSelection === computerSelection) {
-        console.log(TIE_MSG);
-        return playRound(prompt("Make your choice:"), getComputerChoice());
+        divResults.textContent = TIE_MSG;
+        return TIE;
     }
     if (playerSelection === ROCK) {
         if (computerSelection === PAPER) {
-            console.log(LOSE_MSG);
+            divResults.textContent = LOSE_MSG;
             return LOSE;
         }
-        console.log(WIN_MSG)
+        divResults.textContent = WIN_MSG
         return WIN; //rock beats scissors
     }
     if (playerSelection === PAPER) {
         if (computerSelection === SCISSORS) {
-            console.log(LOSE_MSG);
+            divResults.textContent = LOSE_MSG;
             return LOSE;
         }
-        console.log(WIN_MSG)
+        divResults.textContent = WIN_MSG
         return WIN; //Paper beats rock
     }
     if (playerSelection === SCISSORS) {
         if (computerSelection === ROCK) {
-            console.log(LOSE_MSG);
+            divResults.textContent = LOSE_MSG;
             return LOSE;
         }
-        console.log(WIN_MSG)
+        divResults.textContent = WIN_MSG
         return WIN; //scissors beats paper
     }
-    return playRound(
-        prompt("You seem to have made an invalid choice. Please choose again:"),
-        getComputerChoice());   
 }
