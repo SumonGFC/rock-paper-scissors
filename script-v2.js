1a0840884e4d030fe2e865ba2ea2fe62cac32baa
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-01-25 18:55:27 -0500

    Complete game logic!

--- script-v2.js
+++ script-v2.js
@@ -2,7 +2,6 @@ const ROCK = 'rock';
 const PAPER = 'paper';
 const SCISSORS = 'scissors';
 
-
 //functions
 function getComputerChoice() {
     const x = Math.floor(Math.random()*3);
@@ -20,59 +19,84 @@ function playRound(playerSelection, computerSelection) {
     const TIE_MSG = `Tie! We both picked ${playerSelection}! Let's play again.`;
 
     if (playerSelection === computerSelection) {
-        roundResult.textContent = TIE_MSG;
+        roundResultDiv.textContent = TIE_MSG;
         return TIE;
     }
     if (playerSelection === ROCK) {
         if (computerSelection === PAPER) {
-            roundResult.textContent = LOSE_MSG;
+            roundResultDiv.textContent = LOSE_MSG;
             return LOSE;
         }
-        roundResult.textContent = WIN_MSG
+        roundResultDiv.textContent = WIN_MSG
         return WIN; //rock beats scissors
     }
     if (playerSelection === PAPER) {
         if (computerSelection === SCISSORS) {
-            roundResult.textContent = LOSE_MSG;
+            roundResultDiv.textContent = LOSE_MSG;
             return LOSE;
         }
-        roundResult.textContent = WIN_MSG
+        roundResultDiv.textContent = WIN_MSG
         return WIN; //Paper beats rock
     }
     if (playerSelection === SCISSORS) {
         if (computerSelection === ROCK) {
-            roundResult.textContent = LOSE_MSG;
+            roundResultDiv.textContent = LOSE_MSG;
             return LOSE;
         }
-        roundResult.textContent = WIN_MSG
+        roundResultDiv.textContent = WIN_MSG
         return WIN; //scissors beats paper
     }
+}
+
+function decideWinner() {
+    if (Math.max(userScore, computerScore) === ROUNDS) {
+        if (userScore === ROUNDS) {
+            alert("Congrats! You win!");
+            userScore = 0;
+            computerScore = 0;
+            scoreCard.textContent = `You: ${userScore}, Computer: ${computerScore}`;
+            roundResultDiv.textContent = "Let's play again!";
+        } else {
+            alert("You lose. Try better next time");
+            userScore = 0;
+            computerScore = 0;
+            scoreCard.textContent = `You: ${userScore}, Computer: ${computerScore}`;
+            roundResultDiv.textContent = "Let's play again!";
+        }
+    }
+    return;
 }
 
 const btnRock = document.querySelector("#rock");
 const btnPaper = document.querySelector("#paper");
 const btnScissors = document.querySelector("#scissors");
 const scoreCard = document.querySelector("#score-card");
-const roundResult = document.querySelector("#round-result");
+const roundResultDiv = document.querySelector("#round-result");
 
+const ROUNDS = 5;
 let userScore = 0;
 let computerScore = 0;
 
+let roundResult;
+
 btnRock.addEventListener("click", () => {
-    result = playRound(ROCK, getComputerChoice());
-    userScore += result[0];
-    computerScore += result[1];
+    roundResult = playRound(ROCK, getComputerChoice());
+    userScore += roundResult[0];
+    computerScore += roundResult[1];
     scoreCard.textContent = `You: ${userScore}, Computer: ${computerScore}`;
+    decideWinner();
 });
 btnPaper.addEventListener("click", () => {
-    result = playRound(PAPER, getComputerChoice())
-    userScore += result[0];
-    computerScore += result[1];
+    roundResult = playRound(PAPER, getComputerChoice())
+    userScore += roundResult[0];
+    computerScore += roundResult[1];
     scoreCard.textContent = `You: ${userScore}, Computer: ${computerScore}`;
+    decideWinner();
 });
 btnScissors.addEventListener("click", () => {
-    result = playRound(SCISSORS, getComputerChoice())
-    userScore += result[0];
-    computerScore += result[1];
+    roundResult = playRound(SCISSORS, getComputerChoice())
+    userScore += roundResult[0];
+    computerScore += roundResult[1];
     scoreCard.textContent = `You: ${userScore}, Computer: ${computerScore}`;
+    decideWinner();
 });
